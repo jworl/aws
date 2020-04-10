57a0182c302a736cc715e93bdd41640c63d7108d
Joshua Worley <joshua.worley@turner.com>
2020-04-10 14:47:38 -0400

    progress

--- spinup.sh
+++ spinup.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
 ACCT="--profile aws-wmcso-globalprotect-prod:aws-wmcso-globalprotect-prod-admin --region $1"
+ZONE=$2
+AVZN=$1$2
 EC2="aws ec2"
 
 # echo ${ACCT}
 
 _EXISTS(){
-    if [[ $(echo "$1" | jq '.SecurityGroups | length') -eq 1 ]]; then
+    if [[ $(echo "$1" | jq ".$2 | length") -eq 1 ]]; then
       echo "0"
     else
       echo "1"
@@ -17,14 +19,25 @@ VPC=`${EC2} describe-vpcs ${ACCT} --filters "Name=isDefault,Values=true" | jq '.
 VPCID=$(echo ${VPC} | jq -r '.VpcId')
 
 
-
-SUBNET=`${EC2} describe-subnets ${ACCT} --filters "Name=availability-zone,Values=us-east-1a"`
+SUBNET=`${EC2} describe-subnets ${ACCT} --filters "Name=availability-zone,Values=${AVZN}"`
+SNSTATE=$(echo ${SUBNET} | jq -r ".Subnets[].State")
+if [[ ${SNSTATE} != "available" ]]; then 
+    echo "${AVZN} is ${SNSTATE}"; exit 2
+else
+    SNID=$(echo ${SUBNET} | jq -r .Subnets[].SubnetId)
+    echo "${AVZN} subnet-id is ${SNID}"
+fi
 
 SGMGMT=`${EC2} describe-security-groups ${ACCT} --filters "Name=group-name,Values=panos-mgmt"`
+if [[ $(_EXISTS "${SGMGMT}" "SecurityGroups") -eq 1 ]]; then
+    echo "missing SG for MGMT"; exit 2
+else
+    SGMGMTID=$(echo ${SGMGMT} | jq -r .SecurityGroups[].GroupId)
+    echo "GroupId for MGMT is ${SGMGMTID}"
+fi
+
 SGGP=`${EC2} describe-security-groups ${ACCT} --filters "Name=group-name,Values=globalprotect"`
-for v in SGMGMT SGGP; do
-    if [[ $(_EXISTS "${$(echo ${v})}") -eq 0 ]]; then
-        echo "Missing ${v} profile; terminating..."
-        exit 2
-    fi
-done
+if [[ $(_EXISTS "${SGGP}" "SecurityGroups") -eq 1 ]]; then echo "missing SG for GlobalProtect"; exit 2; fi
+
+NIMGMT=`${EC2} describe-network-interfaces ${ACCT} --filters "Name=availability-zone,Values=${AVZN}" "Name=group-id,Values=${SGMGMTID}"`
+echo ${NIMGMT} | jq .
